e70db5b38a396cd07b343a284bbd8319fc748a6f
Jonathan Trinidad <athantrinidad@gmail.com>
2017-10-12 15:23:54 +0800

    Change Nav Titles

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -24,6 +24,7 @@
                             <ul class="nav navbar-nav navbar-right">
                                 <li class="active"><a href="#intro">Home</a></li>
                                 <li><a href="#about">About</a></li>
+                                <li><a href="#service">Book a Ride</a></li>
                                 <li><a href="#works">Contact</a></li>				                                                                  
 
                                 <li><a href="#contact">Contact</a></li>
